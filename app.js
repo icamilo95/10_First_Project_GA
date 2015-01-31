549717bf6a3379dd6b8f9906ec8d84f350f881b4
Camilo Ordonez <icoj@gmail.com>
2015-01-31 15:41:43 -0800

    js adds products v1

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 $(document).ready(function() { 
 
 
+	function addProducts (){
+		contProducts = 1;
+		$('.product-1').on("click", function (e){
+			var productCode = parseInt(this.id);   //Takes the number out of the string to compare with amount-result number
+			for (var i = 0; i < 9; i++) {
+				if (productCode === i) {
+					var result = String("#" +[i] + "-result");	
+					$(result).text(contProducts++);
+				}
+			}
+		});
+	}
 
 
 
 
 
 
-
-
-
-
+	addProducts();
  });
