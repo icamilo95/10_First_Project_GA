89b825c39fbb75e8507fc60a2a918abdf5ef8d0b
Camilo Ordonez <icoj@gmail.com>
2015-01-31 15:51:57 -0800

    js adds products v2

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ $(document).ready(function() {
 
 	function addProducts (){
 		contProducts = 1;
+		oldProdcutCode = 0;
 		$('.product-1').on("click", function (e){
 			var productCode = parseInt(this.id);   //Takes the number out of the string to compare with amount-result number
+			
+			if (productCode !== oldProdcutCode) {
+				oldProdcutCode = productCode;
+				contProducts = 1;
+			}
+			
+
 			for (var i = 0; i < 9; i++) {
 				if (productCode === i) {
 					var result = String("#" +[i] + "-result");	
