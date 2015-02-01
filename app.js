b1c8635b07ffa2892874f156587081606b7d9d97
Camilo Ordonez <icoj@gmail.com>
2015-01-31 22:38:17 -0800

    totalizer created

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
 
 function addingProductsToList () {
 
-	ProductsList = {};
-	amountOfProducts = 0;
+	var newProductsList = {};
+	var inventoryList = {1:10,2:20,3:30,4:40,5:50,6:10,7:20,8:30,9:40,10:50};
+	var totalPerProduct = {};
+	
 	checkForProduct();	
+	
 
 	function checkForProduct (){
 		$('.product').on("click", function (e){
-		var productCode = parseInt(this.id);   //Takes the number out of the string to compare with amount-result number
+		var productCode = parseInt(this.id);   
 		addNewProdcutToList(productCode);	
 		});		
 		$('.positive').on("click", function (e){
@@ -17,22 +20,22 @@ function addingProductsToList () {
 		});
 		$('.negative').on("click", function (e){
 		var productCode = parseInt(this.id);
-		console.log(productCode);
 		subtractProdcutFromList(productCode);	
 		});
 	}	
 
 	function addNewProdcutToList (productCode){		  
-		if (productCode in ProductsList === false) {
-			ProductsList[productCode] = 1;	
+		if (productCode in newProductsList === false) {
+			newProductsList[productCode] = 1;	
 		} else {
-			ProductsList[productCode] += 1;
+			newProductsList[productCode] += 1;
 		} displayAmount(productCode);
+		
 	}		
 
 	function subtractProdcutFromList (productCode){		  
-		if (ProductsList[productCode] > 0) {
-			ProductsList[productCode] -= 1;
+		if (newProductsList[productCode] > 0) {
+			newProductsList[productCode] -= 1;
 		 	displayAmount(productCode);
 		}
 	}			
@@ -42,17 +45,44 @@ function addingProductsToList () {
 			for (var i = 0; i < 11; i++) {
 				if (productCode === i) {
 					var result = String("#" +[i] + "-result");	
-					$(result).text(ProductsList[productCode]);
-				
+					$(result).text(newProductsList[productCode]);
+					totalize();
 				}
 			}
 	}
+
+	function totalize () {
+		for (var clientProduct in newProductsList) {
+			var pricePerProduct = inventoryList[clientProduct];
+			var amountPerProduct = newProductsList[clientProduct]; 
+			totalPerProduct[clientProduct] = pricePerProduct * amountPerProduct;
+		}  
+		var totalToCart = 0;
+  		for( var productCode in totalPerProduct ) {
+    		if( totalPerProduct.hasOwnProperty(productCode)) {
+    			totalToCart += parseFloat( totalPerProduct[productCode] );
+    		}
+ 		}
+  		console.log(totalToCart);
+  		$(".totalHolder").attr("placeholder",totalToCart);
+	}
+
+	
+
+
+
+
+	// function Product (quantity,price){
+	// 	this.quantity = quantity;
+	// 	this.price = price;
+	// }
+
+	// var bread = new Product (20,25);
+	// var milk = new Product (10,10);
+
+
+totalize();
 }	
+addingProductsToList();
 
-
-
-
-
-
-	addingProductsToList();
- });
+});
